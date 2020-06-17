607e4ef525d21e0dc37f549872fff6337e951747
Ron MacNeil <macro@hotmail.com>
2020-06-17 21:58:00 +1000

    Switch to more normal code formatting

--- MacroConsole/ReadmeFilter.cs
+++ MacroConsole/ReadmeFilter.cs
@@ -1,59 +1,51 @@
 using System;
 using System.Collections.Generic;
 
-
-namespace
-MacroConsole
+namespace MacroConsole
 {
 
-
-public static class
-ReadmeFilter
-{
-
-
-/// <summary>
-/// Select only certain sections from a Markdown readme file
-/// </summary>
-///
-public static IEnumerable<string>
-SelectSections(IEnumerable<string> lines, params string[] sections)
-{
-    if (lines == null) throw new ArgumentNullException("lines");
-    if (sections == null) throw new ArgumentNullException("sections");
-    return SelectSectionsImpl(lines, new HashSet<string>(sections, StringComparer.OrdinalIgnoreCase));
-}
-
-
-static IEnumerable<string>
-SelectSectionsImpl(IEnumerable<string> lines, ISet<string> sections)
-{
-    string lastLine = null;
-    bool selected = false;
-    foreach (var line in lines)
+    public static class ReadmeFilter
     {
-        var trimmedLine = line.Trim();
-        if (trimmedLine.Length > 0 && trimmedLine == new string('=', trimmedLine.Length))
+
+        /// <summary>
+        /// Select only certain sections from a Markdown readme file
+        /// </summary>
+        ///
+        public static IEnumerable<string> SelectSections(IEnumerable<string> lines, params string[] sections)
         {
-            if (lastLine == null) continue;
-            selected = sections.Contains(lastLine);
-            if (selected)
-            {
-                yield return lastLine;
-                yield return line;
-            }
-            lastLine = null;
-            continue;
+            if (lines == null) throw new ArgumentNullException("lines");
+            if (sections == null) throw new ArgumentNullException("sections");
+            return SelectSectionsImpl(lines, new HashSet<string>(sections, StringComparer.OrdinalIgnoreCase));
         }
 
-        if (lastLine != null && selected) yield return lastLine;
 
-        lastLine = line;
+        static IEnumerable<string> SelectSectionsImpl(IEnumerable<string> lines, ISet<string> sections)
+        {
+            string lastLine = null;
+            bool selected = false;
+            foreach (var line in lines)
+            {
+                var trimmedLine = line.Trim();
+                if (trimmedLine.Length > 0 && trimmedLine == new string('=', trimmedLine.Length))
+                {
+                    if (lastLine == null) continue;
+                    selected = sections.Contains(lastLine);
+                    if (selected)
+                    {
+                        yield return lastLine;
+                        yield return line;
+                    }
+                    lastLine = null;
+                    continue;
+                }
+
+                if (lastLine != null && selected) yield return lastLine;
+
+                lastLine = line;
+            }
+
+            if (lastLine != null && selected) yield return lastLine;
+        }
+
     }
-
-    if (lastLine != null && selected) yield return lastLine;
-}
-
-
-}
 }
--- MacroConsole/ShadowCopier.cs
+++ MacroConsole/ShadowCopier.cs
@@ -1,91 +1,81 @@
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 
-
-namespace
-MacroConsole
+namespace MacroConsole
 {
 
-
-public static class
-ShadowCopier
-{
-
-
-const string
-ShadowDirName = "_shadow";
-
-
-/// <summary>
-/// "Shadow copy" the calling program's binaries to a shadow subdirectory
-/// </summary>
-///
-/// <remarks>
-/// <para>
-/// Actually, move the calling program's running binaries to a <c>_shadow</c> subdirectory, maintaining unlocked copies
-/// in the original locations which can be replaced or removed while the program is still running.
-/// </para>
-/// <para>
-/// Prior to moving, all assemblies statically referenced by the calling program are preloaded so the running process
-/// binds to them, otherwise it may lazy load and bind after the copy is perfomed, defeating the purpose.
-/// </para>
-/// <para>
-/// Shortcomings:
-/// - Program requires write access to its binaries' location on disk
-/// - The copy is shallow, it does not recurse into subdirectories
-/// - Probably fails if more than one instance of the program is run at the same time, because both processes would try
-///   to use the same shadow subdirectory
-/// </para>
-/// </remarks>
-///
-public static void
-ShadowCopy()
-{
-    var callingAssembly = Assembly.GetCallingAssembly();
-
-    PreloadReferencedAssemblies(callingAssembly);
-
-    var originalDir = Path.GetDirectoryName(callingAssembly.Location);
-    var shadowDir = Path.Combine(originalDir, ShadowDirName);
-    if (Directory.Exists(shadowDir)) Directory.Delete(shadowDir, true);
-    Directory.CreateDirectory(shadowDir);
-
-    foreach (var path in Directory.EnumerateFiles(originalDir))
+    public static class ShadowCopier
     {
-        var name = Path.GetFileName(path);
-        var shadowPath = Path.Combine(shadowDir, name);
-        File.Move(path, shadowPath);
+
+        const string ShadowDirName = "_shadow";
+
+
+        /// <summary>
+        /// "Shadow copy" the calling program's binaries to a shadow subdirectory
+        /// </summary>
+        ///
+        /// <remarks>
+        /// <para>
+        /// Actually, move the calling program's running binaries to a <c>_shadow</c> subdirectory, maintaining unlocked
+        /// copies in the original locations which can be replaced or removed while the program is still running.
+        /// </para>
+        /// <para>
+        /// Prior to moving, all assemblies statically referenced by the calling program are preloaded so the running
+        /// process binds to them, otherwise it may lazy load and bind after the copy is perfomed, defeating the
+        /// purpose.
+        /// </para>
+        /// <para>
+        /// Shortcomings:
+        /// - Program requires write access to its binaries' location on disk
+        /// - The copy is shallow, it does not recurse into subdirectories
+        /// - Probably fails if more than one instance of the program is run at the same time, because both processes
+        ///   would try to use the same shadow subdirectory
+        /// </para>
+        /// </remarks>
+        ///
+        public static void ShadowCopy()
+        {
+            var callingAssembly = Assembly.GetCallingAssembly();
+
+            PreloadReferencedAssemblies(callingAssembly);
+
+            var originalDir = Path.GetDirectoryName(callingAssembly.Location);
+            var shadowDir = Path.Combine(originalDir, ShadowDirName);
+            if (Directory.Exists(shadowDir)) Directory.Delete(shadowDir, true);
+            Directory.CreateDirectory(shadowDir);
+
+            foreach (var path in Directory.EnumerateFiles(originalDir))
+            {
+                var name = Path.GetFileName(path);
+                var shadowPath = Path.Combine(shadowDir, name);
+                File.Move(path, shadowPath);
+            }
+
+            foreach (var path in Directory.EnumerateFiles(shadowDir))
+            {
+                var name = Path.GetFileName(path);
+                var originalPath = Path.Combine(originalDir, name);
+                File.Copy(path, originalPath);
+            }
+        }
+
+
+        static void PreloadReferencedAssemblies(Assembly assembly)
+        {
+            PreloadReferencedAssemblies(assembly, new HashSet<string>());
+        }
+
+
+        static void PreloadReferencedAssemblies(Assembly assembly, ISet<string> loaded)
+        {
+            foreach (var assemblyName in assembly.GetReferencedAssemblies())
+            {
+                if (loaded.Contains(assemblyName.Name)) continue;
+                loaded.Add(assemblyName.Name);
+                PreloadReferencedAssemblies(Assembly.Load(assemblyName), loaded);
+            }
+        }
+
     }
-
-    foreach (var path in Directory.EnumerateFiles(shadowDir))
-    {
-        var name = Path.GetFileName(path);
-        var originalPath = Path.Combine(originalDir, name);
-        File.Copy(path, originalPath);
-    }
-}
-
-
-static void
-PreloadReferencedAssemblies(Assembly assembly)
-{
-    PreloadReferencedAssemblies(assembly, new HashSet<string>());
-}
-
-
-static void
-PreloadReferencedAssemblies(Assembly assembly, ISet<string> loaded)
-{
-    foreach (var assemblyName in assembly.GetReferencedAssemblies())
-    {
-        if (loaded.Contains(assemblyName.Name)) continue;
-        loaded.Add(assemblyName.Name);
-        PreloadReferencedAssemblies(Assembly.Load(assemblyName), loaded);
-    }
-}
-
-
-}
 }
